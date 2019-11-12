2e2010746f3ec9c74a5e83dfa7c52ae225afd1a4
shravan318 <shravan.318@gmail.com>
2019-11-12 09:51:24 +0530

    add col-6 t the repo display sections

--- ui.js
+++ ui.js
@@ -39,10 +39,10 @@ class UI {
       output += `
           <div class="card card-body mb-2">
           <div class="row">
-          <div class="col6">
+          <div class="col-6">
           <a href="${repo.html_url}" target="_blank">${repo.name}</a>
           </div>
-          <div class="col6">
+          <div class="col-6">
           <span class="badge badge-primary">Stars: ${repo.stargazers_count}</span>
             <span class="badge badge-secondary">Watchers: ${repo.watchers_count}</span>
             <span class="badge badge-success">Forks: ${repo.forms_count}</span>
